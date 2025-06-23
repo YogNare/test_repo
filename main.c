6e624652b24a1e91971ed639612c876465485f08
YogNare <mysticnare@gmail.com>
2025-06-24 02:18:50 +0700

    feat: подпись коммита через ssh

--- main.c
+++ main.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
 
-
 int main() {
 
-  printf("Hope, it's work\n");
-
-  int a;
-  scanf("%d", &a);
-
-  printf("%d", a);
-  
-  printf("fucking peace of shit\n");
+  printf("hello world\n");
 
   return 0;
 }
